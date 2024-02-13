91fde5792718b0d9dc75eb83f11df3c2f73ccf20
Benaouda Rafik <gokub531@gmail.com>
2024-02-13 20:55:10 +0100

    small change to the departements component

--- src/components/DepartementElement.tsx
+++ src/components/DepartementElement.tsx
@@ -9,7 +9,7 @@ interface Props {
 const DepartementElement = ({rotate , color , children} :Props ) => {
   const {ref,animate} = useAnimate()
   return (
-    <div ref={ref} className={` flex items-center justify-center bg-slate-100 w-[200px] h-[200px] ${color} rounded-full border-8 border-l-0  rotate-${rotate} ${animate && "spin-once"} `} >
+    <div ref={ref} className={` shadow-lg flex items-center justify-center bg-slate-100 w-[200px] h-[200px] ${color} rounded-full border-8 border-l-0  rotate-${rotate} ${animate && "spin-once"} `} >
       <p className={`text-lg font-semibold text-slate-700 -rotate-${rotate}` } > {children} </p> 
     </div>
   )
--- src/components/Departements.tsx
+++ src/components/Departements.tsx
@@ -1,15 +1,17 @@
 import DepartementElement from "./DepartementElement"
-
+import think from "../assets/think.jpg"
 const Departements = () => {
   return (
     <section id="departements" >
       <h2 className=" text-lg sm:text-2xl text-slate-500 font-medium text-center mt-4 " >Departements</h2>
         <div className="p-1 flex flex-col gap-14 mx-auto  items-center py-8 px-2 relative md:flex-row md:justify-evenly" >
-            <DepartementElement color="border-red-500" rotate="45" > Media </DepartementElement>
+            <DepartementElement color="border-green-500" rotate="45" > Media </DepartementElement>
             <DepartementElement color="border-blue-500" rotate="90" > Logestic </DepartementElement>
-            <DepartementElement color="border-green-500" rotate="180" > Community </DepartementElement>
-            <DepartementElement color="border-red-500" rotate="12" > Software Engineering </DepartementElement>
-
+            <DepartementElement color="border-red-500" rotate="180" > Software Engineering </DepartementElement>
+        </div>
+        <div className=" max-w-[400px] flex flex-col items-center mx-auto " >
+          <h3 className="text-slate-500 text-lg font-medium text-center mb-4" >Which one would you join?</h3>
+        <img className="w-40" src={think} alt="gama thinking" />
         </div>
     </section>
     )
